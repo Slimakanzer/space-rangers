daa79708b17bb328197059ece78cea083c5706c7
cactus666 <43724414+cactus666@users.noreply.github.com>
2018-10-04 18:43:36 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -5,16 +5,6 @@
 -- Dumped from database version 10.5 (Ubuntu 10.5-0ubuntu0.18.04)
 -- Dumped by pg_dump version 10.5 (Ubuntu 10.5-0ubuntu0.18.04)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
